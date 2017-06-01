c2678b3d75ff2db7882afd3ff188170ecee6da20
Raphael Grewe <raphael.grewe@greenbone.net>
2017-06-01 09:17:48 +0200

    version 1.0.1

--- clients/gvm_pyshell.py
+++ clients/gvm_pyshell.py
@@ -31,7 +31,7 @@ from libs.gvm_connection import (SSHConnection,
                                  TLSConnection,
                                  UnixSocketConnection)
 
-__version__ = '1.0'
+__version__ = '1.0.1'
 
 logger = logging.getLogger(__name__)
 
