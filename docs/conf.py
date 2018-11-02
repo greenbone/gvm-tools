d18c03838e5829b45af16cd044a2e9fdb11cc600
Björn Ricks <bjoern.ricks@greenbone.net>
2018-11-02 11:11:53 +0100

    Add favicon to docs

--- docs/conf.py
+++ docs/conf.py
@@ -81,6 +81,8 @@ pygments_style = None
 #
 html_theme = 'alabaster'
 
+html_favicon = 'favicon.gif'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
