d3cba85bc03e0b68657710a2857a3fb7125db8c6
jstach <jstach@uos.de>
2019-10-25 13:23:38 +0200

    Renaming of class Suppress to SuppressOutput

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,7 +20,7 @@ import sys
 import os
 
 
-class Suppress:
+class SuppressOutput:
     def __init__(self, *, suppress_stdout=False, suppress_stderr=False):
         self.suppress_stdout = suppress_stdout
         self.suppress_stderr = suppress_stderr
