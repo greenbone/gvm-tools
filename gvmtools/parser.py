77d9e68d1c16828add518f59219470fdc25ceb8e
Korkmatik <fahri.korkmaz@protonmail.ch>
2020-11-01 11:40:13 +0100

    Refactored creation of SSHConnection

--- gvmtools/parser.py
+++ gvmtools/parser.py
@@ -333,18 +333,13 @@ def create_connection(
             cafile=cafile,
         )
 
+    ssh_connection_args = {
+        'timeout': timeout,
+        'hostname': hostname,
+        'username': ssh_username,
+        'password': ssh_password,
+    }
     if port is not None:
-        return SSHConnection(
-            timeout=timeout,
-            hostname=hostname,
-            port=port,
-            username=ssh_username,
-            password=ssh_password,
-        )
-    else:
-        return SSHConnection(
-            timeout=timeout,
-            hostname=hostname,
-            username=ssh_username,
-            password=ssh_password,
-        )
+        ssh_connection_args['port'] = port
+
+    return SSHConnection(**ssh_connection_args)
