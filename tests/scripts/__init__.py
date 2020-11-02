a25e07a407cc52a17c3a87629865b8fe7927656b
Jaspar Löchte <jaspar.loechte@greenbone.net>
2020-11-02 21:58:22 +0100

    First test for send-tasks script

---
+++ tests/scripts/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest.mock import patch, MagicMock, create_autospec
+from lxml import etree
+
+from gvm.errors import GvmResponseError
+from gvm.protocols.gmpv9 import Gmp
+
+
+class GmpMockFactory:
+    def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+        gmp_protocol_mock = create_autospec(Gmp)
+
+        self.gmp_protocol = gmp_protocol_mock
+        self.gmp = MagicMock()
+        self.gmp.is_authenticated = MagicMock(return_value=True)
+        self.gmp.__enter__.return_value = gmp_protocol_mock
+
+    def __call__(self, *args, **kwargs):
+        return self.gmp
+
+    def mock_response(self, request_name: str, content: str):
+        func = getattr(self.gmp_protocol, request_name)
+        func.return_value = etree.fromstring(content)
