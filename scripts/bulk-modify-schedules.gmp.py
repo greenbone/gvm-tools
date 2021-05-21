e3a05b6d5d8c7f2a2f59e6700a89921bdf4cca3c
Timo Pollmeier <timo.pollmeier@greenbone.net>
2021-05-21 15:20:44 +0200

    Add "import sys" to bulk-modify-schedules.gmp.py

--- scripts/bulk-modify-schedules.gmp.py
+++ scripts/bulk-modify-schedules.gmp.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
 
 def check_args(args):
     len_args = len(args.script) - 1
