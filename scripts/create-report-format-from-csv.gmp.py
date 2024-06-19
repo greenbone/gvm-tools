020ea58551d3ec996bcd9cbe003715a4b3954cfc
Björn Ricks <bjoern.ricks@greenbone.net>
2024-06-19 10:22:59 +0200

    Change: Update copyright headers of all scripts
    Use new and current copyright header format.

--- scripts/create-report-format-from-csv.gmp.py
+++ scripts/create-report-format-from-csv.gmp.py
@@ -1,28 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-# Loosely based on the create-targets-from-host-list.gmp.py
-# As provided by Greenbone in the gvm-tools repo
-#
-# Martin Boller
+# SPDX-FileCopyrightText: 2024 Martin Boller
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 # Run with gvm-script --gmp-username admin-user --gmp-password password socket create-report_formats-from-csv.gmp.py report_formats.csv
-#
-#
 
 import sys
 import time
--- scripts/export-csv-report.gmp.py
+++ scripts/export-csv-report.gmp.py
@@ -1,31 +1,9 @@
-# -*- coding: utf-8 -*-
-# 
-# Based on the Greenbone export-pdf-report script and modified to 
-# create csv and return more (all) details.
-# Martin Boller
-#
-# Copyright (C) 2019-2021 Greenbone Networks GmbH
+# SPDX-FileCopyrightText: 2024 Martin Boller
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# 2022-11-02 - Martin B
-# Added ignore_pagination=True, details=True to get the full report
-# 
 
-import sys
+# Based on the export-pdf-report script and modified to
+# create csv and return more (all) details.
 
 from base64 import b64decode
 from pathlib import Path
