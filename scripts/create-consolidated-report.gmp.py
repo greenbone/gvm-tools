279a31bd066d8ff3cabf22c262efa0988ef2ce0c
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2024-11-12 14:54:59 +0100

    Fix linting errors

--- scripts/create-consolidated-report.gmp.py
+++ scripts/create-consolidated-report.gmp.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import sys
 from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
 from datetime import date
 from typing import List, Tuple
