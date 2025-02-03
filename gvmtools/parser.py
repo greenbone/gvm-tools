de5e872c1acfc3b7754132fc0c903acdf4dfd3c3
Björn Ricks <bjoern.ricks@greenbone.net>
2025-02-03 08:31:13 +0100

    Update formatting required for new black version
    Satisfy black auto formatter.

--- gvmtools/parser.py
+++ gvmtools/parser.py
@@ -16,8 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Command Line Interface Parser
-"""
+"""Command Line Interface Parser"""
 
 import argparse
 import logging
